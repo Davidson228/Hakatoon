e39e86a30631b2f520b4ea687a4eff2be2aa3296
Davidson228 <31989135+Davidson228@users.noreply.github.com>
2019-03-13 16:05:36 +0200

    wd

--- oldriga.php
+++ oldriga.php
@@ -5,3 +5,6 @@ $app->initLayout('Centered');
 
 $image = $app->add(['Image','https://www.likealocalguide.com/media/cache/0c/07/0c071343953aed5a2c3f6fa47484482c.jpg']);
 $text = $app->add(['Text','The Dome Cathedral, Lutheran Church of St. Peter, St. Jacobs Cathedral, Big Guild, Small Guild, Swedish Gate and more you can visit in the Old Riga!']);
+
+$back = $riga->add(['Button','Oldriga']);
+$back ->link(['index']);
