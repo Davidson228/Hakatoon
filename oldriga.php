72beb53c82bf7e0407205b68db7b2c6c5d8dd29d
Davidson228 <31989135+Davidson228@users.noreply.github.com>
2019-03-13 15:30:16 +0200

    test

--- oldriga.php
+++ oldriga.php
@@ -3,4 +3,4 @@ require 'vendor/autoload.php';
 $app = new \atk4\ui\App('On a trip around Latvia');
 $app->initLayout('Centered');
 
-$image = $app->add(['Image','http://www.citariga.lv/images/vietas/peterabaznica_b.jpg']);
+$image = $app->add(['Image','https://www.latvia.travel/sites/default/files/styles/lightbox/public/tourism_sight/riga-old-town-latvia-travel_10.jpg?itok=Dc93KQ2-']);
