567f31c1762abaea3c9139e106c6353d242c4f33
Davidson228 <31989135+Davidson228@users.noreply.github.com>
2019-03-13 15:24:21 +0200

    test

--- oldriga.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = new \atk4\ui\App('On a trip around Latvia');
-$app->initLayout('Centered');
-$menu = $app->add('Menu');
-
-$image = $app->add(['Image','https://www.likealocalguide.com/media/cache/0c/07/0c071343953aed5a2c3f6fa47484482c.jpg']);
