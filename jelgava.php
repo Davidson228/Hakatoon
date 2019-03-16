ba26b89df2eb15645f91300af2ef65be31dc9450
Davidson228 <31989135+Davidson228@users.noreply.github.com>
2019-03-16 19:57:37 +0200

    trs

--- jelgava.php
+++ jelgava.php
@@ -5,7 +5,7 @@ session_start();
 //$app = new \atk4\ui\App($_SESSION['city']);
 $app = new \atk4\ui\App('Jelgava');
 $app->initLayout('Centered');
-$img = 'http://www.liaa.gov.lv/files/liaa/content/ENG/Business_Locations/007_ml.jpg';
+$img = 'http://www.infotop.lv/uploads/img/articles/large/%D0%B0%D1%8B%D1%86%D0%B0%D1%8B%D1%84%D0%B0%D1%8B%D0%B0%D1%84%D1%8B%D0%B0%D1%8B%D1%84%D0%B0%D1%84%D1%8B%D0%B0_946231165.jpg';
 $image = $app->add(['Image',$img]);
 $button1=$app->add(['Button','BACK']);
 $button1->link('index.php');
