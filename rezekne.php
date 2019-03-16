0cab2637f618c3ec52a26b79a01e8acb7df2e09a
Davidson228 <31989135+Davidson228@users.noreply.github.com>
2019-03-16 20:01:29 +0200

    test

---
+++ rezekne.php
@@ -0,0 +1,11 @@
+<?php
+require 'vendor/autoload.php';
+session_start();
+//$_SESSION['city']=$_GET['city'];
+//$app = new \atk4\ui\App($_SESSION['city']);
+$app = new \atk4\ui\App('Rezekne');
+$app->initLayout('Centered');
+$img = 'https://baltnews.lv/images/102233/63/1022336310.jpg';
+$image = $app->add(['Image',$img]);
+$button1=$app->add(['Button','BACK']);
+$button1->link('index.php');
